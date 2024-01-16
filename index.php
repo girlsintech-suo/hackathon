e1aeb54e10cf53b56b03f462e1a5d0d1b0f6e5be
Tithi Soni <tithi2k3@gmail.com>
2024-01-15 22:46:25 -0800

    uploaded home, css, and fonts

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Hackathon</title>
+    </head>
+    <body>
+        <nav>
+            <a href="index.php">HOME</a>
+            <a href="registration.html">ReGISTRATION</a>
+            <a href="sponsorship.html">SPONSORSHIP</a>
+            <a href="aboutus.html">ABOUT US</a>
+        </nav>
+    </body>
+</html>
